ba07feae2444a490a389cdeca6dd5f6108b660a4
Bread Dev <114232869+Tushiri@users.noreply.github.com>
2023-11-05 20:51:48 +0800

    initial commit

---
+++ js/flowers.js
@@ -0,0 +1,5 @@
+//hehe dagdag files lang HAHAHAHA//
+
+onload = () =>{
+    document.body.classList.remove("container");
+};
---
+++ js/playlist.js
@@ -0,0 +1,20 @@
+let songs = [
+	{
+	name: "Your Song",
+	artist: "Parokya ni Edgar",
+	img: "your_song",
+	audio: "Your Song"
+	},
+	{
+	name: "It's Not Living (If It's Not With You)",
+	artist: "The 1975",
+	img: "The 1975",
+	audio: "It's Not Living"
+	},
+	{
+	name: "An Art Gallery Could Never Be As Unique As You",
+	artist: "MRLD",
+	img: "MRLD",
+	audio: "mrld - An Art Gallery Could Never Be As Unique As You (Official Audio)"
+	}
+]
